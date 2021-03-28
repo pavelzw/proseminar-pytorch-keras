969bcbd07b2126f51362512ad5a836ac707e5cce
Pavel Zwerschke <uctxx@student.kit.edu>
2021-03-29 00:05:00 +0200

    Small fixes

--- ausarbeitung/code/functional-keras.py
+++ ausarbeitung/code/functional-keras.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Flatten, Dense
 from tensorflow.keras.models import Model
 
-inputs = Input(shape=(image_size, image_size, 3))
-h = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(inputs)
+inp = Input(shape=(image_size, image_size, 3))
+h = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(inp)
 h = MaxPool2D(pool_size=(2,2), strides=2)(h)
 h = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(h)
 h = MaxPool2D(pool_size=(2,2), strides=2)(h)
